64183b6a4cf07ae86635761a113d4e9eca965d56
BilalRanglerz12 <bilal.ranglerz199@gmail.com>
2024-08-06 16:48:46 +0500

    changes

--- resources/views/admin/adminnotification/index.blade.php
+++ resources/views/admin/adminnotification/index.blade.php
@@ -100,14 +100,14 @@
         <section class="section">
             <div class="section-body">
                 <div class="row">
-                    <div class="col-12 col-md-12 col-lg-12">
+                    <div class="col-6 col-md-6 col-lg-6">
                         <div class="card">
                             <div class="card-header">
                                 <div class="col-12">
                                     <h4>Notification</h4>
                                 </div>
                             </div>
-                            <div class="card-body table-responsive">
+                            <div class="card-body table-responsive text-center">
                                 <a class="btn btn-primary mb-3 text-white" data-toggle="modal"
                                     data-target="#createAdminNotificationModal">
                                     Create Notification
