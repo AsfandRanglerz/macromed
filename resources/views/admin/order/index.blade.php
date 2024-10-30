57495e5958725ae2fac63a2b0559f97ea75332e5
BilalRanglerz12 <bilal.ranglerz199@gmail.com>
2024-10-30 17:15:43 +0500

    changes

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -107,7 +107,7 @@
     <!-- Modal -->
     <div class="modal fade" id="selectSalesAgentModal" tabindex="-1" aria-labelledby="selectSalesAgentModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="selectSalesAgentModalLabel">Select Sales Agent</h5>
