f1610ae71b0988bc831daa1920646c136725a96a
BilalRanglerz12 <bilal.ranglerz199@gmail.com>
2024-11-13 12:49:40 +0500

    changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
         <section class="section">
             <div class="row mb-3">
                 <div class="col-xl-3 mb-3 col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                    <div class="card h-100">
+                    <div class="card">
                         <div class="card-statistic-4">
                             <div class="align-items-center justify-content-between">
                                 <div class="row ">
@@ -14,7 +14,7 @@
                                         <div class="card-content">
                                             <h5 class="font-15">New Booking</h5>
                                             <h2 class="mb-3 font-18">258</h2>
-                                            <p class="mb-0"><span class="col-green">10%</span> Increase</p>
+                                            {{-- <p class="mb-0"><span class="col-green">10%</span> Increase</p> --}}
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -28,7 +28,7 @@
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 mb-3 col-md-6 col-sm-6 col-xs-12">
-                    <div class="card h-100">
+                    <div class="card">
                         <div class="card-statistic-4">
                             <div class="align-items-center justify-content-between">
                                 <div class="row ">
@@ -36,7 +36,7 @@
                                         <div class="card-content">
                                             <h5 class="font-15"> Customers</h5>
                                             <h2 class="mb-3 font-18">1,287</h2>
-                                            <p class="mb-0"><span class="col-orange">09%</span> Decrease</p>
+                                            {{-- <p class="mb-0"><span class="col-orange">09%</span> Decrease</p> --}}
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -50,7 +50,7 @@
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 mb-3 col-md-6 col-sm-6 col-xs-12">
-                    <div class="card h-100">
+                    <div class="card">
                         <div class="card-statistic-4">
                             <div class="align-items-center justify-content-between">
                                 <div class="row ">
@@ -58,8 +58,8 @@
                                         <div class="card-content">
                                             <h5 class="font-15">New Project</h5>
                                             <h2 class="mb-3 font-18">128</h2>
-                                            <p class="mb-0"><span class="col-green">18%</span>
-                                                Increase</p>
+                                            {{-- <p class="mb-0"><span class="col-green">18%</span> --}}
+                                                {{-- Increase</p> --}}
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
@@ -73,7 +73,7 @@
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-6 mb-3 col-md-6 col-sm-6 col-xs-12">
-                    <div class="card h-100">
+                    <div class="card">
                         <div class="card-statistic-4">
                             <div class="align-items-center justify-content-between">
                                 <div class="row ">
@@ -81,7 +81,7 @@
                                         <div class="card-content">
                                             <h5 class="font-15">Revenue</h5>
                                             <h2 class="mb-3 font-18">$48,697</h2>
-                                            <p class="mb-0"><span class="col-green">42%</span> Increase</p>
+                                            {{-- <p class="mb-0"><span class="col-green">42%</span> Increase</p> --}}
                                         </div>
                                     </div>
                                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 pl-0">
